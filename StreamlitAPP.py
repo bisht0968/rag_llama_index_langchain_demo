0426b1d58b656b4cf51f4c9dc32eabf996f9a1d4
Rahul <rahubisht0968@gmail.com>
2024-07-23 19:24:52 +0530

    Train model once

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import traceback
-from backend.src.rag_llama_index_langchain_backend.model import response_generator
+from backend.src.rag_llama_index_langchain_backend.model import response_generator,train_model
 
 st.title("Rag Application using llama_index and langchain")
+
+
+@st.cache_resource
+def get_trained_model():
+    return train_model()
+
+query_engine = get_trained_model()
 
 with st.form("user inputs"):
     user_query = st.text_input("Enter Your Question related to Attention is all you need Or YOLO")
@@ -11,7 +18,7 @@ with st.form("user inputs"):
     if button is not None and user_query:
         with st.spinner("Loading......"):
             try:
-                final_response = response_generator(user_query=user_query)
+                final_response = response_generator(query_engine=query_engine,user_query=user_query)
             except Exception as e:
                 traceback.print_exception(type(e),e,e.__traceback__)
                 st.error("Error")
--- backend/src/rag_llama_index_langchain_backend/model.py
+++ backend/src/rag_llama_index_langchain_backend/model.py
@@ -6,20 +6,17 @@ from llama_index.core import VectorStoreIndex
 load_dotenv()
 
 HUGGINGFACE_API_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
-def response_generator(user_query):
 
+def train_model():
     documents = extract_pdfs("pdfs")
-
     embed_model = embedding_model("sentence-transformers/all-mpnet-base-v2")
-
     llm = initiliaze_llm(HUGGINGFACE_API_TOKEN)
-
     service_context = service_context_model(embed_model = embed_model,llm = llm,chunk_size = 500,chunk_overlap = 150)
-
     index = VectorStoreIndex.from_documents(documents,service_context = service_context)
-
     query_engine = index.as_query_engine()
+    return query_engine
 
+def response_generator(query_engine,user_query):
     formatted_query = query_formatting(user_query=user_query)
 
     response = query_engine.query(formatted_query)
